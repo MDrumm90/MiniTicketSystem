b553659fc95cfbbd3f2989b846261d6790cb5cea
MDrumm90 <michal.drummond@gmail.com>
2026-09-05 19:38:25 +0200

    Add domain project, add ticket  and ticket status domain models.

---
+++ MiniTicketSystem.Domain/Entities/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using MiniTicketSystem.Domain.Enum;
+
+namespace MiniTicketSystem.Domain.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public TicketStatus Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateClosed { get; set; }
+    }
+}
---
+++ MiniTicketSystem.Domain/Enum/TicketStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniTicketSystem.Domain.Enum
+{
+    public enum TicketStatus
+    {
+        Open,
+        InProgress,
+        Closed
+    }
+}
