9e7f1105a0c9fa9d8eb571221c492caa93840cd5
MDrumm90 <michal.drummond@gmail.com>
2026-09-05 19:51:03 +0200

    Remove duplicated ticket model

--- MiniTicketSystem.Api/Controllers/TicketsController.cs
+++ MiniTicketSystem.Api/Controllers/TicketsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MiniTicketSystem.Domain.Entities;
 
 namespace MiniTicketSystem.Api.Controllers
 {
--- MiniTicketSystem.Api/Ticket.cs
+++
@@ -1,6 +0,0 @@
-namespace MiniTicketSystem.Api
-{
-    public class Ticket
-    {
-    }
-}
